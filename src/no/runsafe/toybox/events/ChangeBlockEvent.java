1e805175e3e7527e494e8840c694f59fd9e06329
Kruithne <kruithne@gmail.com>
2013-04-30 05:19:31 +0100

    Added some debug

--- src/no/runsafe/toybox/events/ChangeBlockEvent.java
+++ src/no/runsafe/toybox/events/ChangeBlockEvent.java
@@ -1,6 +1,7 @@
 package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.entity.IEntityChangeBlockEvent;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.entity.RunsafeLivingEntity;
 import no.runsafe.framework.server.material.RunsafeMaterial;
@@ -8,18 +9,24 @@ import no.runsafe.toybox.handlers.CarePackageHandler;
 
 public class ChangeBlockEvent implements IEntityChangeBlockEvent
 {
-	public ChangeBlockEvent(CarePackageHandler handler)
+	public ChangeBlockEvent(CarePackageHandler handler, IOutput output)
 	{
 		this.handler = handler;
+		this.output = output;
 	}
 
 	@Override
 	public void OnEntityChangeBlockEvent(RunsafeLivingEntity entity, RunsafeBlock block, RunsafeMaterial material)
 	{
+		this.output.write("Block change event fired: " + block.getTypeId());
 		Integer entityID = entity.getEntityId();
 		if (this.handler.CheckDrop(entityID))
+		{
+			this.output.write("CHEST DETECTED");
 			this.handler.ProcessDrop(entityID, block);
+		}
 	}
 
 	private CarePackageHandler handler;
+	private IOutput output;
 }
