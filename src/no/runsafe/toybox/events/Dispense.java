663863974b14c81f855b4b091c77b2312d0f96a7
Kruithne <kruithne@gmail.com>
2013-04-30 18:38:07 +0100

    Complete rewrite to be more efficient and easier to use.

--- src/no/runsafe/toybox/events/Dispense.java
+++ src/no/runsafe/toybox/events/Dispense.java
@@ -2,52 +2,41 @@ package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.block.IBlockDispense;
 import no.runsafe.framework.output.IOutput;
-import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.block.RunsafeBlockState;
 import no.runsafe.framework.server.block.RunsafeDispenser;
-import no.runsafe.framework.server.block.RunsafeSign;
+import no.runsafe.framework.server.inventory.RunsafeInventory;
 import no.runsafe.framework.server.item.RunsafeItemStack;
+import no.runsafe.framework.server.material.RunsafeMaterialData;
+import org.bukkit.material.MaterialData;
 
 public class Dispense implements IBlockDispense
 {
-	public Dispense(IOutput output)
-	{
-		this.output = output;
-	}
-
 	@Override
 	public boolean OnBlockDispense(RunsafeBlock block, RunsafeItemStack item)
 	{
-		RunsafeLocation location = block.getLocation();
-		this.check(block, location, 0, 1, 0);
-		this.check(block, location, 0, -1, 0);
-		this.check(block, location, 1, 0, 0);
-		this.check(block, location, -1, 0, 0);
-		this.check(block, location, 0, 0, 1);
-		this.check(block, location, 0, 0, -1);
+		RunsafeBlockState blockState = block.getBlockState();
+		if (blockState instanceof RunsafeDispenser)
+		{
+			RunsafeDispenser dispenser = (RunsafeDispenser) blockState;
+			RunsafeInventory inventory = dispenser.getInventory();
+			String title = inventory.getTitle();
 
+			if (title.startsWith("Infinite:"))
+			{
+				String[] data = title.split(":");
+				if (data.length > 1)
+				{
+					int itemID = Integer.valueOf(data[1]);
+					RunsafeItemStack items = new RunsafeItemStack(itemID, 64, (short) 0);
+
+					if (data.length > 2)
+						items.setData(new RunsafeMaterialData(itemID, Byte.valueOf(data[2])));
+
+					inventory.addItems(items);
+				}
+			}
+		}
 		return true;
 	}
-
-	private void check(RunsafeBlock block, RunsafeLocation location, int x, int y, int z)
-	{
-		location.offset(x, y, z);
-		RunsafeBlockState blockState = location.getBlock().getBlockState();
-		if (blockState instanceof RunsafeSign)
-		{
-			this.output.write("Found sign around the dispenser");
-			RunsafeSign sign = (RunsafeSign) blockState;
-			if (sign.getLine(0).equals("[Infinite]"))
-				this.fillDispenser(block, Integer.valueOf(sign.getLine(1)));
-		}
-	}
-
-	private void fillDispenser(RunsafeBlock block, Integer itemID)
-	{
-		RunsafeDispenser dispenser = (RunsafeDispenser) block.getBlockState();
-		dispenser.getInventory().addItems(new RunsafeItemStack(itemID, 64, (short) 0));
-	}
-
-	private IOutput output;
 }
