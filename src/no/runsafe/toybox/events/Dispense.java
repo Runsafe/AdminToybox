c46423c628e1926ce2b44bc349373219e5d0a948
Kruithne <kruithne@gmail.com>
2013-04-30 18:07:25 +0100

    Added back in side checks

--- src/no/runsafe/toybox/events/Dispense.java
+++ src/no/runsafe/toybox/events/Dispense.java
@@ -19,10 +19,13 @@ public class Dispense implements IBlockDispense
 	@Override
 	public boolean OnBlockDispense(RunsafeBlock block, RunsafeItemStack item)
 	{
-		this.output.write("Dispense event procced");
 		RunsafeLocation location = block.getLocation();
 		this.check(block, location, 0, 1, 0);
 		this.check(block, location, 0, -1, 0);
+		this.check(block, location, 1, 0, 0);
+		this.check(block, location, -1, 0, 0);
+		this.check(block, location, 0, 0, 1);
+		this.check(block, location, 0, 0, -1);
 
 		return true;
 	}
@@ -31,22 +34,17 @@ public class Dispense implements IBlockDispense
 	{
 		location.offset(x, y, z);
 		RunsafeBlockState blockState = location.getBlock().getBlockState();
-		this.output.write(String.format("Checking sign at %s, %s, %s", x, y, z));
 		if (blockState instanceof RunsafeSign)
 		{
 			this.output.write("Found sign around the dispenser");
 			RunsafeSign sign = (RunsafeSign) blockState;
 			if (sign.getLine(0).equals("[Infinite]"))
-			{
-				this.output.write("Sign is infinite");
 				this.fillDispenser(block, Integer.valueOf(sign.getLine(1)));
-			}
 		}
 	}
 
 	private void fillDispenser(RunsafeBlock block, Integer itemID)
 	{
-		this.output.write("Filling dispenser with a stack of " + itemID);
 		RunsafeDispenser dispenser = (RunsafeDispenser) block.getBlockState();
 		dispenser.getInventory().addItems(new RunsafeItemStack(itemID, 64, (short) 0));
 	}
