f9a7e485966fb7d695d7fae68107e6eb5183ea79
Kruithne <kruithne@gmail.com>
2013-04-30 06:12:33 +0100

    Redesigned function to cancel native code and place custom chest.

--- src/no/runsafe/toybox/events/ChangeBlockEvent.java
+++ src/no/runsafe/toybox/events/ChangeBlockEvent.java
@@ -2,32 +2,34 @@ package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.entity.IEntityChangeBlockEvent;
 import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.event.entity.RunsafeEntityChangeBlockEvent;
 import no.runsafe.toybox.handlers.CarePackageHandler;
+import org.bukkit.Material;
 
 public class ChangeBlockEvent implements IEntityChangeBlockEvent
 {
-	public ChangeBlockEvent(CarePackageHandler handler, IOutput output)
+	public ChangeBlockEvent(CarePackageHandler handler)
 	{
 		this.handler = handler;
-		this.output = output;
 	}
 
 	@Override
 	public void OnEntityChangeBlockEvent(RunsafeEntityChangeBlockEvent event)
 	{
-		RunsafeBlock changedBlock = event.getBlock();
-		RunsafeBlock chest = changedBlock.getWorld().getBlockAt(changedBlock.getLocation());
 		Integer entityID = event.getEntity().getEntityId();
 
-		this.output.write("Block type: " + chest.getTypeId());
-		this.output.write("Block class: " + chest.getClass().getName());
-
 		if (this.handler.CheckDrop(entityID))
-			this.handler.ProcessDrop(entityID, chest);
+		{
+			RunsafeLocation blockLocation = event.getBlock().getLocation();
+			blockLocation.getBlock().setTypeId(Material.CHEST.getId());
+
+			this.handler.ProcessDrop(entityID, blockLocation.getBlock());
+			event.setCancelled(true);
+		}
+
 	}
 
 	private CarePackageHandler handler;
-	private IOutput output;
 }
