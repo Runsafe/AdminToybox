238eb3d9181c77cccc833af6db30ee2b6d84eb7b
Kruithne <kruithne@gmail.com>
2013-04-30 04:50:04 +0100

    Added debug to chest close event

--- src/no/runsafe/toybox/events/ChestClose.java
+++ src/no/runsafe/toybox/events/ChestClose.java
@@ -1,22 +1,29 @@
 package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.inventory.IChestClosed;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.inventory.RunsafeInventory;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.toybox.handlers.CarePackageHandler;
 
+import java.util.logging.Level;
+
 public class ChestClose implements IChestClosed
 {
-	public ChestClose(CarePackageHandler handler)
+	public ChestClose(CarePackageHandler handler, IOutput output)
 	{
 		this.handler = handler;
+		this.output = output;
 	}
 
 	@Override
 	public void OnChestClosed(RunsafePlayer player, RunsafeInventory inventory)
 	{
-		this.handler.DropPackage(player);
+		this.output.outputDebugToConsole("Detected chest close by " + player.getName(), Level.FINE);
+		if (this.handler.PlayerHasOpenCarePackage(player))
+			this.handler.DropPackage(player);
 	}
 
 	private CarePackageHandler handler;
+	private IOutput output;
 }
