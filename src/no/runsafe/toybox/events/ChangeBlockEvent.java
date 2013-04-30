1fca285f5513841b48192b6a1a7eee9bda20cede
Kruithne <kruithne@gmail.com>
2013-04-30 05:58:11 +0100

    Fixed block change detection to get the block after it was changed.

--- src/no/runsafe/toybox/events/ChangeBlockEvent.java
+++ src/no/runsafe/toybox/events/ChangeBlockEvent.java
@@ -2,28 +2,27 @@ package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.entity.IEntityChangeBlockEvent;
 import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.event.entity.RunsafeEntityChangeBlockEvent;
 import no.runsafe.toybox.handlers.CarePackageHandler;
 
 public class ChangeBlockEvent implements IEntityChangeBlockEvent
 {
-	public ChangeBlockEvent(CarePackageHandler handler, IOutput output)
+	public ChangeBlockEvent(CarePackageHandler handler)
 	{
 		this.handler = handler;
-		this.output = output;
 	}
 
 	@Override
 	public void OnEntityChangeBlockEvent(RunsafeEntityChangeBlockEvent event)
 	{
-		this.output.write("Entity change event detected: " + event.getBlock().getTypeId());
-		this.output.write("Block type: " + event.getBlock().getClass().getName());
+		RunsafeBlock changedBlock = event.getBlock();
+		RunsafeBlock chest = changedBlock.getWorld().getBlockAt(changedBlock.getLocation());
 		Integer entityID = event.getEntity().getEntityId();
 
 		if (this.handler.CheckDrop(entityID))
-			this.handler.ProcessDrop(entityID, event.getBlock());
+			this.handler.ProcessDrop(entityID, chest);
 	}
 
 	private CarePackageHandler handler;
-	private IOutput output;
 }
