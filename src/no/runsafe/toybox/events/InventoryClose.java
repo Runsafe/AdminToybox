3ccb4c18ead45b0583140c38a88a3fe6b18aaebd
Kruithne <kruithne@gmail.com>
2013-05-01 19:19:55 +0100

    Added ArrowBarrage frame, updated InventoryClose to match framework changes.

--- src/no/runsafe/toybox/events/InventoryClose.java
+++ src/no/runsafe/toybox/events/InventoryClose.java
@@ -1,6 +1,7 @@
 package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.inventory.IInventoryClosed;
+import no.runsafe.framework.server.event.inventory.RunsafeInventoryCloseEvent;
 import no.runsafe.framework.server.inventory.RunsafeInventory;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.toybox.handlers.CarePackageHandler;
@@ -13,11 +14,12 @@ public class InventoryClose implements IInventoryClosed
 	}
 
 	@Override
-	public void OnInventoryClosed(RunsafePlayer player, RunsafeInventory inventory)
+	public void OnInventoryClosed(RunsafeInventoryCloseEvent event)
 	{
+		RunsafePlayer player = event.getPlayer();
 		if (this.handler.PlayerHasOpenCarePackage(player))
 			this.handler.DropPackage(player);
 	}
-
+	
 	private CarePackageHandler handler;
 }
