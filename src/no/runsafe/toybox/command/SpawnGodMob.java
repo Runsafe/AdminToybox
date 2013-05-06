867e57042de0953fbc8db8540ee6eb685c277b22
Kruithne <kruithne@gmail.com>
2013-05-06 21:54:57 +0100

    Updated enchantments to apply correctly, gave him a sword.

--- src/no/runsafe/toybox/command/SpawnGodMob.java
+++ src/no/runsafe/toybox/command/SpawnGodMob.java
@@ -31,10 +31,11 @@ public class SpawnGodMob extends PlayerCommand
 				RunsafeLivingEntity livingEntity = (RunsafeLivingEntity) entity;
 				RunsafeEntityEquipment equipment = livingEntity.getEquipment();
 
-				equipment.setBoots(this.makeGodItem(313));
-				equipment.setChestplate(this.makeGodItem(311));
-				equipment.setHelmet(this.makeGodItem(310));
-				equipment.setLeggings(this.makeGodItem(312));
+				equipment.setBoots(this.makeGodArmor(313));
+				equipment.setChestplate(this.makeGodArmor(311));
+				equipment.setHelmet(this.makeGodArmor(310));
+				equipment.setLeggings(this.makeGodArmor(312));
+				equipment.setItemInHand(this.makeGodSword(276));
 
 				livingEntity.addPotionEffect(RunsafePotionEffect.create(RunsafePotionEffectType.INCREASE_DAMAGE, 1200, 5));
 				livingEntity.addPotionEffect(RunsafePotionEffect.create(RunsafePotionEffectType.FIRE_RESISTANCE, 1200, 5));
@@ -45,25 +46,26 @@ public class SpawnGodMob extends PlayerCommand
 		return null;
 	}
 
-	private RunsafeItemStack makeGodItem(int itemID)
+	private RunsafeItemStack makeGodArmor(int itemID)
 	{
 		RunsafeItemStack item = new RunsafeItemStack(itemID);
-		item.addEnchantment(RunsafeEnchantment.ARROW_DAMAGE, 5);
-		item.addEnchantment(RunsafeEnchantment.ARROW_FIRE, 5);
-		item.addEnchantment(RunsafeEnchantment.ARROW_INFINITE, 5);
-		item.addEnchantment(RunsafeEnchantment.ARROW_DAMAGE, 5);
-		item.addEnchantment(RunsafeEnchantment.ARROW_KNOCKBACK, 5);
-		item.addEnchantment(RunsafeEnchantment.DAMAGE_ALL, 5);
-		item.addEnchantment(RunsafeEnchantment.DAMAGE_ARTHROPODS, 5);
-		item.addEnchantment(RunsafeEnchantment.DAMAGE_UNDEAD, 5);
 		item.addEnchantment(RunsafeEnchantment.DURABILITY, 5);
-		item.addEnchantment(RunsafeEnchantment.FIRE_ASPECT, 5);
-		item.addEnchantment(RunsafeEnchantment.KNOCKBACK, 5);
 		item.addEnchantment(RunsafeEnchantment.PROTECTION_ENVIRONMENTAL, 5);
 		item.addEnchantment(RunsafeEnchantment.PROTECTION_FALL, 5);
 		item.addEnchantment(RunsafeEnchantment.PROTECTION_FIRE, 5);
 		item.addEnchantment(RunsafeEnchantment.PROTECTION_PROJECTILE, 5);
 		item.addEnchantment(RunsafeEnchantment.THORNS, 5);
+		return item;
+	}
+
+	private RunsafeItemStack makeGodSword(int itemID)
+	{
+		RunsafeItemStack item = new RunsafeItemStack(itemID);
+		item.addEnchantment(RunsafeEnchantment.FIRE_ASPECT, 5);
+		item.addEnchantment(RunsafeEnchantment.DAMAGE_ALL, 5);
+		item.addEnchantment(RunsafeEnchantment.DURABILITY, 5);
+		item.addEnchantment(RunsafeEnchantment.KNOCKBACK, 5);
+		item.addEnchantment(RunsafeEnchantment.DAMAGE_ALL, 5);
 		return item;
 	}
 }
