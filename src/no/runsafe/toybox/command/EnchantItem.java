a841fe9f4d06104f5e67d307d918c51896a5373b
Kruithne <kruithne@gmail.com>
2013-05-07 06:10:26 +0100

    Reworked item enchanting command.

--- src/no/runsafe/toybox/command/EnchantItem.java
+++ src/no/runsafe/toybox/command/EnchantItem.java
@@ -7,7 +7,6 @@ import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.toybox.handlers.Enchanter;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 public class EnchantItem extends PlayerCommand
@@ -30,38 +29,22 @@ public class EnchantItem extends PlayerCommand
 		RunsafeItemStack item = executor.getItemInHand();
 		String enchantName = parameters.get("enchant");
 
-		try
+		if (enchantName.equalsIgnoreCase("all"))
 		{
-			if (enchantName.equalsIgnoreCase("all"))
-			{
-				this.enchanter.applyAllEnchants(item);
-				return "&2Applied all available enchants to your item.";
-			}
-			else
-			{
-				Field[] fields = RunsafeEnchantmentType.class.getDeclaredFields();
-
-				for (Field field : fields)
-				{
-					if (field.getName().equalsIgnoreCase(enchantName.toUpperCase()))
-					{
-						RunsafeEnchantmentType type = (RunsafeEnchantmentType) field.get(RunsafeEnchantmentType.class);
-						RunsafeEnchantment enchant = new RunsafeEnchantment(type);
-
-						if (enchant.canEnchantItem(item))
-						{
-							int power = (arguments.length > 0 ? Integer.valueOf(arguments[0]) : enchant.getMaxLevel());
-							item.addEnchantment(enchant, power);
-							return "&2Your item has been enchanted.";
-						}
-					}
-				}
-			}
-			return "&cThat enchant cannot be applied to that item.";
+			this.enchanter.applyAllEnchants(item);
+			return "&2Applied all available enchants to your item.";
 		}
-		catch (Exception e)
+		else
 		{
-			return "&cThe enchant you are looking for does not exist.";
+			RunsafeEnchantmentType type = RunsafeEnchantmentType.valueOf(enchantName);
+			if (type == null)
+				return "&cThat enchant cannot be applied to that item.";
+
+			RunsafeEnchantment enchant = new RunsafeEnchantment(type);
+			int power = (arguments.length > 0 ? Integer.valueOf(arguments[0]) : enchant.getMaxLevel());
+			item.addEnchantment(enchant, power);
+
+			return "&2Your item has been enchanted.";
 		}
 	}
 
