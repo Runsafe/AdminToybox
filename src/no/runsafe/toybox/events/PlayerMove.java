bb0ec9b53bb04b1bd37cea962818fad4e00e5c38
Morten Nilsen <morten@runsafe.no>
2013-06-08 23:13:04 +0200

    Disuse deprecated method

--- src/no/runsafe/toybox/events/PlayerMove.java
+++ src/no/runsafe/toybox/events/PlayerMove.java
@@ -1,17 +1,17 @@
 package no.runsafe.toybox.events;
 
 import no.runsafe.framework.event.player.IPlayerMove;
+import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Effect;
-import org.bukkit.Material;
 
 public class PlayerMove implements IPlayerMove
 {
 	@Override
 	public boolean OnPlayerMove(RunsafePlayer player, RunsafeLocation from, RunsafeLocation to)
 	{
-		if (to.getBlock().getMaterialType().getMaterialId() == 154)
+		if (to.getBlock().is(Item.Redstone.Device.Hopper))
 		{
 			player.damage(20);
 			player.getWorld().playEffect(player.getLocation(), Effect.POTION_BREAK, 16421);
