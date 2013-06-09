0a50403c1e2b4b17c2d5ae70a8c5691b7c991866
Kruithne <kruithne@gmail.com>
2013-06-09 07:50:11 +0100

    Revert: Use new book API (2cbc737)

--- src/no/runsafe/toybox/command/Author.java
+++ src/no/runsafe/toybox/command/Author.java
@@ -2,8 +2,8 @@ package no.runsafe.toybox.command;
 
 import no.runsafe.framework.command.player.PlayerCommand;
 import no.runsafe.framework.minecraft.Item;
-import no.runsafe.framework.server.item.RunsafeBook;
 import no.runsafe.framework.server.item.RunsafeItemStack;
+import no.runsafe.framework.server.item.meta.RunsafeBookMeta;
 import no.runsafe.framework.server.player.RunsafePlayer;
 
 import java.util.HashMap;
@@ -21,8 +21,10 @@ public class Author extends PlayerCommand
 		RunsafeItemStack item = executor.getItemInHand();
 		if (item.is(Item.Special.Crafted.WrittenBook))
 		{
+			RunsafeBookMeta meta = (RunsafeBookMeta) item.getItemMeta();
 			String playerName = parameters.get("player");
-			((RunsafeBook) item).setAuthor(playerName);
+			meta.setAuthor(playerName);
+			item.setItemMeta(meta);
 			return "&2Author changed to " + playerName;
 		}
 		return "&cYou cannot change the author of that item.";
