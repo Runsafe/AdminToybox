caa09fc2551cb3df59069fedad57513a261a4b40
Kruithne <kruithne@gmail.com>
2013-06-09 18:24:01 +0100

    Code clean-up + proper framework support for droppers

--- src/no/runsafe/toybox/events/Dispense.java
+++ src/no/runsafe/toybox/events/Dispense.java
@@ -4,9 +4,9 @@ import no.runsafe.framework.event.block.IBlockDispense;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.block.RunsafeBlockState;
 import no.runsafe.framework.server.block.RunsafeDispenser;
+import no.runsafe.framework.server.block.RunsafeDropper;
 import no.runsafe.framework.server.inventory.RunsafeInventory;
 import no.runsafe.framework.server.item.meta.RunsafeMeta;
-import org.bukkit.block.Dropper;
 
 public class Dispense implements IBlockDispense
 {
@@ -14,24 +14,17 @@ public class Dispense implements IBlockDispense
 	public boolean OnBlockDispense(RunsafeBlock block, RunsafeMeta item)
 	{
 		RunsafeBlockState blockState = block.getBlockState();
+		RunsafeInventory inventory = null;
+
 		if (blockState instanceof RunsafeDispenser)
-		{
-			RunsafeDispenser dispenser = (RunsafeDispenser) blockState;
-			RunsafeInventory inventory = dispenser.getInventory();
-			String title = inventory.getTitle();
+			inventory = ((RunsafeDispenser) blockState).getInventory();
+		else if (blockState instanceof RunsafeDropper)
+			inventory = ((RunsafeDropper) blockState).getInventory();
 
-			if (title.equalsIgnoreCase("Infinite"))
+		if (inventory != null)
+			if (inventory.getTitle().equalsIgnoreCase("Infinite"))
 				inventory.addItems(item.clone());
-		}
-		else if (blockState.getRaw() instanceof Dropper)
-		{
-			Dropper dropper = (Dropper) blockState.getRaw();
-			RunsafeInventory inventory = new RunsafeInventory(dropper.getInventory());
-			String title = inventory.getTitle();
 
-			if (title.equalsIgnoreCase("Infinite"))
-				inventory.addItems(item.clone());
-		}
 		return true;
 	}
 }
