0073499a165dfd6d14c98ce64cc33b72b911c47f
Kruithne <kruithne@gmail.com>
2013-07-06 21:36:49 +0100

    Added error catching for IllegalArgumentException.

--- src/no/runsafe/toybox/command/SpawnHorse.java
+++ src/no/runsafe/toybox/command/SpawnHorse.java
@@ -26,20 +26,27 @@ public class SpawnHorse extends PlayerCommand
 	@Override
 	public String OnExecute(RunsafePlayer executor, HashMap<String, String> parameters, String[] arguments)
 	{
-		SpawnableHorseType type = (arguments.length > 0 ? SpawnableHorseType.valueOf(arguments[0].toUpperCase()) : this.getRandomHorseType());
-		SpawnableHorseVariant variant = (arguments.length > 1 ? SpawnableHorseVariant.valueOf(arguments[1].toUpperCase()) : this.getRandomHorseVariant());
-
-		if (type == null)
-			return "&cInvalid horse type.";
-
-		if (variant == null)
-			return "&cInvalid horse variant.";
-
-		int count = Integer.valueOf(parameters.get("count"));
-
-		for (int i = 0; i < count; ++i)
+		try
 		{
-			this.horseSpawner.spawnHorse(executor.getLocation(), type, variant, parameters.get("tame").equals("1"));
+			SpawnableHorseType type = (arguments.length > 0 ? SpawnableHorseType.valueOf(arguments[0].toUpperCase()) : this.getRandomHorseType());
+			SpawnableHorseVariant variant = (arguments.length > 1 ? SpawnableHorseVariant.valueOf(arguments[1].toUpperCase()) : this.getRandomHorseVariant());
+
+			if (type == null)
+				return "&cInvalid horse type.";
+
+			if (variant == null)
+				return "&cInvalid horse variant.";
+
+			int count = Integer.valueOf(parameters.get("count"));
+
+			for (int i = 0; i < count; ++i)
+			{
+				this.horseSpawner.spawnHorse(executor.getLocation(), type, variant, parameters.get("tame").equals("1"));
+			}
+		}
+		catch (IllegalArgumentException exception)
+		{
+			return "&cThat type of horse does not exist.";
 		}
 
 		return null;
