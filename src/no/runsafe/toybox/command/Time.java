cd5f1b8e40f7accacc0f450dd3ff4c0fcd578eb9
Morten Nilsen <morten@runsafe.no>
2013-12-12 23:57:45 +0100

    Full implementation

--- src/no/runsafe/toybox/command/Time.java
+++ src/no/runsafe/toybox/command/Time.java
@@ -1,27 +1,42 @@
 package no.runsafe.toybox.command;
 
+import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.command.ExecutableCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.command.argument.EnumArgument;
 import no.runsafe.framework.api.command.argument.WorldArgument;
-import no.runsafe.framework.api.log.IDebug;
+import no.runsafe.framework.api.log.IConsole;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.internal.Multiverse;
 
 import java.util.Map;
 
 public class Time extends ExecutableCommand
 {
-	public Time(IDebug debug)
+	public Time(IConsole console)
 	{
-		super("time", "Change the time in a world", "runsafe.toybox.time", new WorldArgument(true), new EnumArgument("time", WellKnownTimes.values(), true));
-		this.debug = debug;
+		super("time", "Change the time in a world", "runsafe.toybox.time", new EnumArgument("time", WellKnownTimes.values(), true), new WorldArgument(false));
+		this.console = console;
 	}
 
 	@Override
 	public String OnExecute(ICommandExecutor executor, Map<String, String> parameters)
 	{
+		IWorld target = null;
+		if (parameters.containsKey("world"))
+			target = Multiverse.Get().getWorld(parameters.get("world"));
+
+		if (target == null && executor instanceof IPlayer)
+			target = ((IPlayer) executor).getWorld();
+
+
+		if (target == null)
+			return "You must specify a world from the console!";
+
 		int time = WellKnownTimes.valueOf(parameters.get("time")).getTime();
-		debug.debugFine("Setting time for %s to %s..", parameters.get("world"), time);
-		return null;
+		target.setTime(time);
+		console.logInformation("Time in world %s set to %d by %s", target.getName(), time, executor.getName());
+		return String.format("Time set for %s", target.getName());
 	}
 
 	public enum WellKnownTimes
@@ -41,5 +56,5 @@ public class Time extends ExecutableCommand
 		private final int time;
 	}
 
-	private final IDebug debug;
+	private final IConsole console;
 }
