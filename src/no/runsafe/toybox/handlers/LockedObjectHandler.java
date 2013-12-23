2584afa45f97da91063aabb82339294376fe4f4c
Kruithne <kruithne@gmail.com>
2013-12-23 03:41:36 +0000

    Corrected NPE issue on load.

--- src/no/runsafe/toybox/handlers/LockedObjectHandler.java
+++ src/no/runsafe/toybox/handlers/LockedObjectHandler.java
@@ -99,7 +99,8 @@ public class LockedObjectHandler implements IPluginEnabled, IPluginDisabled, IBl
 		List<ILocation> locations = this.repository.getLockedObjects();
 		for (ILocation location : locations)
 		{
-			if (this.canLockBlock(location.getBlock()))
+			IBlock block = location.getBlock();
+			if (block != null && this.canLockBlock(block))
 			{
 				String worldName = location.getWorld().getName();
 				if (!this.lockedObjects.containsKey(worldName))
