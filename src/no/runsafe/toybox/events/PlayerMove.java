c97d063bb16df23e7a053c99d2f19a9b95051d7c
Kruithne <kruithne@gmail.com>
2013-12-30 20:32:19 +0000

    Remove unused import.

--- src/no/runsafe/toybox/events/PlayerMove.java
+++ src/no/runsafe/toybox/events/PlayerMove.java
@@ -5,7 +5,6 @@ import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.event.player.IPlayerMove;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.Item;
-import no.runsafe.framework.minecraft.RunsafeWorld;
 import org.bukkit.Effect;
 
 public class PlayerMove implements IPlayerMove
