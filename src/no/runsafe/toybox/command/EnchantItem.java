9bc0855a64b5f2d03a1057a8506b50b28e3287ce
Morten Nilsen <morten@runsafe.no>
2014-02-21 00:02:00 +0100

    For science!

--- src/no/runsafe/toybox/command/EnchantItem.java
+++ src/no/runsafe/toybox/command/EnchantItem.java
@@ -2,7 +2,7 @@ package no.runsafe.toybox.command;
 
 import no.runsafe.framework.api.command.argument.EnchantArgument;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.OptionalArgument;
+import no.runsafe.framework.api.command.argument.ListOf;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.minecraft.IEnchant;
 import no.runsafe.framework.api.player.IPlayer;
@@ -15,7 +15,7 @@ public class EnchantItem extends PlayerCommand
 	{
 		super(
 			"enchant", "Enchants an item", "runsafe.toybox.enchant",
-			new EnchantArgument(true), new OptionalArgument("power")
+			new ListOf<EnchantArgument>(new EnchantArgument(true))
 		);
 	}
 
@@ -25,21 +25,14 @@ public class EnchantItem extends PlayerCommand
 		RunsafeItemStack item = executor.getItemInHand();
 		if (item == null)
 			return "&cNo item in your hand.";
-
-		String enchantName = parameters.get("enchant");
+		String[] enchants = parameters.get("enchant").split(" ");
+		for (String enchantName : enchants)
+		{
 			IEnchant enchant = Enchant.getByName(enchantName);
+			if (enchant != null)
+				enchant.max().applyTo(item);
 
-		if (enchant == null)
-			return "&cThe enchantment you are looking for does not exist. Use '/enchant help' for help.";
-
-		if (!enchant.canEnchant(item))
-			return "&cThat enchant cannot be applied to that item.";
-
-		if (parameters.containsKey("power"))
-			enchant.power(Integer.valueOf(parameters.get("power"))).applyTo(item);
-		else
-			enchant.max().applyTo(item);
-
+		}
 		return "&2Your item has been enchanted.";
 	}
 }
