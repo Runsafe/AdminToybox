d48581428da2c92da0b317e3da09863721b77592
Morten Nilsen <morten@runsafe.no>
2014-02-20 23:34:48 +0100

    Clean up code

--- src/no/runsafe/toybox/command/EnchantItem.java
+++ src/no/runsafe/toybox/command/EnchantItem.java
@@ -1,17 +1,13 @@
 package no.runsafe.toybox.command;
 
+import no.runsafe.framework.api.command.argument.EnchantArgument;
 import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.argument.OptionalArgument;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.minecraft.IEnchant;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.Enchant;
 import no.runsafe.framework.minecraft.item.RunsafeItemStack;
-import org.apache.commons.lang.StringUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EnchantItem extends PlayerCommand
 {
@@ -19,7 +15,7 @@ public class EnchantItem extends PlayerCommand
 	{
 		super(
 			"enchant", "Enchants an item", "runsafe.toybox.enchant",
-			new RequiredArgument("enchant"), new OptionalArgument("power")
+			new EnchantArgument(true), new OptionalArgument("power")
 		);
 	}
 
@@ -31,42 +27,19 @@ public class EnchantItem extends PlayerCommand
 			return "&cNo item in your hand.";
 
 		String enchantName = parameters.get("enchant");
-		if (enchantName.equalsIgnoreCase("help"))
-		{
-			List<String> enchantNames = new ArrayList<String>();
-			for (IEnchant enchant : Enchant.All)
-			{
-				enchantNames.add(
-					String.format(
-						item.getItemId() > 0 ? "%s%s" : "%2$s",
-						enchant.canEnchant(item) ? "&a" : "&c",
-						enchant.getName().toLowerCase()
-					)
-				);
-			}
-			return "&3" + StringUtils.join(enchantNames, "&3, ");
-		}
-		else if (enchantName.equalsIgnoreCase("all"))
-		{
-			item.enchant(Enchant.All);
-			return "&2Applied all available enchants to your item.";
-		}
-		else
-		{
 			IEnchant enchant = Enchant.getByName(enchantName);
 
-			if (enchant == null)
-				return "&cThe enchantment you are looking for does not exist. Use '/enchant help' for help.";
+		if (enchant == null)
+			return "&cThe enchantment you are looking for does not exist. Use '/enchant help' for help.";
 
-			if (!enchant.canEnchant(item))
-				return "&cThat enchant cannot be applied to that item.";
+		if (!enchant.canEnchant(item))
+			return "&cThat enchant cannot be applied to that item.";
 
-			if (parameters.containsKey("power"))
-				enchant.power(Integer.valueOf(parameters.get("power"))).applyTo(item);
-			else
-				enchant.max().applyTo(item);
+		if (parameters.containsKey("power"))
+			enchant.power(Integer.valueOf(parameters.get("power"))).applyTo(item);
+		else
+			enchant.max().applyTo(item);
 
-			return "&2Your item has been enchanted.";
-		}
+		return "&2Your item has been enchanted.";
 	}
 }
