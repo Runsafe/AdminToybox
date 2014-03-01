334ed239564a51b800b2c455dd59079c084f4c55
Morten Nilsen <morten@runsafe.no>
2014-03-01 23:08:35 +0100

    Rework command to work with new API

--- src/no/runsafe/toybox/command/Kill.java
+++ src/no/runsafe/toybox/command/Kill.java
@@ -1,79 +1,57 @@
 package no.runsafe.toybox.command;
 
-import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.ExecutableCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.argument.ListOf;
-import no.runsafe.framework.api.command.argument.OnlinePlayerRequired;
-import no.runsafe.framework.api.player.IAmbiguousPlayer;
+import no.runsafe.framework.api.command.argument.Player;
 import no.runsafe.framework.api.player.IPlayer;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Kill extends ExecutableCommand
 {
-	public Kill(IServer server)
+	public Kill()
 	{
-		super("kill", "Kills the targeted player", "runsafe.toybox.kill", new ListOf<OnlinePlayerRequired>(new OnlinePlayerRequired()));
-		this.server = server;
+		super("kill", "Kills the targeted player", "runsafe.toybox.kill", new ListOf<Player.Online.Required>(new Player.Online.Required()));
 	}
 
 	@Override
 	public String OnExecute(ICommandExecutor executor, IArgumentList parameters)
 	{
-		List<String> hitList = new ArrayList<String>();
-
+		List<IPlayer> players = parameters.getValue("player");
+		if (players == null)
+			return null;
 		if (executor instanceof IPlayer)
 		{
 			IPlayer executingPlayer = (IPlayer) executor;
+			if (players.isEmpty())
+			{
+				return killPlayer(executingPlayer);
+			}
 			if (!executingPlayer.hasPermission("runsafe.toybox.kill.others"))
-			{
-				if (!parameters.containsKey("player"))
-				{
-					executingPlayer.explode(0, false, false);
-					executingPlayer.damage(500.0D);
-					return "&fWell, if you insist.";
-				}
 				return "&cYou do not have permission to kill other players like this.";
-			}
-			else
-			{
-				if (!parameters.containsKey("player"))
-				{
-					executingPlayer.explode(0, false, false);
-					executingPlayer.damage(500.0D);
-					return "&fWell, if you insist.";
-				}
-			}
 		}
 		else
 		{
-			if (!parameters.containsKey("player"))
+			if (players.isEmpty())
 				return "&cPlease provide some players you wish to kill.";
 		}
-		Collections.addAll(hitList, parameters.get("player").split("\\s+"));
-
-		for (String targetName : hitList)
+		for (IPlayer target : players)
 		{
-			IPlayer target = server.getPlayer(targetName);
-
 			if (target == null)
 				continue;
 
-			if (target instanceof IAmbiguousPlayer)
-				return target.toString();
-
 			if (target.isOnline())
-			{
-				target.explode(0, false, false);
-				target.damage(500.0D);
-			}
+				killPlayer(target);
 		}
 		return "&cDeath shall find those you seek quickly.";
 	}
 
-	private final IServer server;
+	private String killPlayer(IPlayer victim)
+	{
+		victim.explode(0, false, false);
+		victim.damage(500.0D);
+		return "&fWell, if you insist.";
+	}
 }
