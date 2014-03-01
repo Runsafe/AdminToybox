f9b352bf8205b7ebe89ce350943b4281847f1a36
Kruithne <kruithne@gmail.com>
2014-03-01 02:03:49 +0000

    Simplify the roll command.

--- src/no/runsafe/toybox/command/Roll.java
+++ src/no/runsafe/toybox/command/Roll.java
@@ -2,7 +2,6 @@ package no.runsafe.toybox.command;
 
 import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.OptionalArgument;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
 
@@ -12,7 +11,7 @@ public class Roll extends PlayerCommand
 {
 	public Roll()
 	{
-		super("roll", "Roll to produce a random number!", "runsafe.toybox.roll", new OptionalArgument("first"), new OptionalArgument("second"));
+		super("roll", "Roll to produce a random number!", "runsafe.toybox.roll");
 	}
 
 	@Override
@@ -23,23 +22,7 @@ public class Roll extends PlayerCommand
 		if (playerLocation == null)
 			return null;
 
-		int low = 0;
-		int high = 100;
-
-		if (parameters.containsKey("first"))
-		{
-			if (parameters.containsKey("second"))
-			{
-				high = Integer.parseInt(parameters.get("second"));
-				low = Integer.parseInt(parameters.get("first"));
-			}
-			else
-			{
-				high = Integer.parseInt(parameters.get("first"));
-			}
-		}
-
-		String message = String.format(executor.getPrettyName() + " &erolls %s (%s-%s).", random.nextInt(high) + low, low, high);
+		String message = executor.getPrettyName() + " &erolls " + random.nextInt(101) + " (0-100).";
 		for (IPlayer targetPlayer : playerLocation.getPlayersInRange(20D))
 			targetPlayer.sendColouredMessage(message);
 
