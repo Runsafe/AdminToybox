7bb1f6decafd35e77e15c96cb63ee6f20564bf7f
njs41 <njs42@hotmail.com>
2017-03-07 16:32:42 -0500

    Fix and improve SpawnCustomMinecart command

--- src/no/runsafe/toybox/command/SpawnCustomMinecart.java
+++ src/no/runsafe/toybox/command/SpawnCustomMinecart.java
@@ -1,6 +1,5 @@
 package no.runsafe.toybox.command;
 
-import net.minecraft.server.v1_8_R3.EntityMinecartAbstract;
 import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.argument.WholeNumber;
 import no.runsafe.framework.api.command.player.PlayerCommand;
@@ -9,24 +8,44 @@ import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.internal.wrapper.ObjectUnwrapper;
 import no.runsafe.framework.minecraft.entity.PassiveEntity;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.material.MaterialData;
 
 public class SpawnCustomMinecart extends PlayerCommand
 {
 	public SpawnCustomMinecart()
 	{
-		super("spawncustomminecart", "Spawn a custom minecart!", "runsafe.toybox.spawnminecart", new WholeNumber("id"), new WholeNumber("data"));
+		super("spawncustomminecart",
+				"Spawn a custom minecart!",
+				"runsafe.toybox.spawnminecart",
+				new WholeNumber("id"),
+				new WholeNumber("data"),
+				new WholeNumber("blockOffset")
+		);
 	}
 
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
+		//Create minecart
 		IEntity minecartEntity = PassiveEntity.Minecart.spawn(executor.getLocation());
 		CraftEntity craftEntity = ObjectUnwrapper.convert(minecartEntity);
-		EntityMinecartAbstract ema = (EntityMinecartAbstract) craftEntity.getHandle();
+		Minecart ema = (Minecart) craftEntity.getHandle();
 
-		ema.k((Integer) parameters.getValue("id"));
-		//ema.l((Integer) parameters.getValue("data"));
-        // TODO: Fix block Data
+		//Create block in minecart
+		if(parameters.getValue("id") == null)
+			return null;
+		MaterialData minecartBlock = new MaterialData(
+				(Integer) parameters.getValue("id"),
+				(byte)(int)(Integer)parameters.getValue("data")
+		);
+		ema.setDisplayBlock(minecartBlock);
+
+		//Set block offset
+		if(parameters.getValue("blockOffset") == null)
+			return null;
+		ema.setDisplayBlockOffset((Integer) parameters.getValue("blockOffset"));
+
 		return null;
 	}
 }
