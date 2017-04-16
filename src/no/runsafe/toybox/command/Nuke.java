624b5805c6ecdc2020a6bd73bcfd59a8fa19b5ce
njs41 <njs42@hotmail.com>
2017-04-16 18:38:35 -0400

    Improve Nuke argument types

--- src/no/runsafe/toybox/command/Nuke.java
+++ src/no/runsafe/toybox/command/Nuke.java
@@ -2,7 +2,7 @@ package no.runsafe.toybox.command;
 
 import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
+import no.runsafe.framework.api.command.argument.WholeNumber;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.entity.IExplosive;
 import no.runsafe.framework.api.player.IPlayer;
@@ -15,8 +15,8 @@ public class Nuke extends PlayerCommand
 		super("nuke",
 			"Nukes in a certain radius",
 			"runsafe.toybox.nuke",
-			new RequiredArgument(RADIUS),
-			new RequiredArgument(POWER)
+			new WholeNumber(RADIUS).require(),
+			new WholeNumber(POWER).require()
 		);
 	}
 
@@ -27,12 +27,12 @@ public class Nuke extends PlayerCommand
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
-		int radius = Integer.valueOf(parameters.get(RADIUS));
+		int radius = parameters.getValue(RADIUS);
 
 		if (radius > 15)
 			return "Max radius size: 15";
 
-		int power = Integer.valueOf(parameters.get(POWER));
+		int power = parameters.getValue(POWER);
 		ILocation location = executor.getLocation();
 
 		location.decrementX(radius);
