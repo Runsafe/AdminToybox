0be91cdf2bc7c78de23762800c60fecf647131f4
DevDeborah <softwaredeveloperdeborah@gmail.com>
2024-09-02 19:28:44 -0400

    Update enchant command to be closer to vanilla behavior
    Most players running the command expect it to only apply one enchant at a time, set it to do that behavior along with allowing the player to set a custom enchant level

--- src/no/runsafe/toybox/command/EnchantItem.java
+++ src/no/runsafe/toybox/command/EnchantItem.java
@@ -2,14 +2,11 @@ package no.runsafe.toybox.command;
 
 import no.runsafe.framework.api.command.argument.Enchant;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.ListOf;
+import no.runsafe.framework.api.command.argument.WholeNumber;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.minecraft.IEnchant;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.item.RunsafeItemStack;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class EnchantItem extends PlayerCommand
 {
@@ -17,11 +14,12 @@ public class EnchantItem extends PlayerCommand
 	{
 		super(
 			"enchant", "Enchants an item", "runsafe.toybox.enchant",
-			new ListOf<>(new Enchant(ENCHANT)).require()
+			new Enchant(ENCHANT).require(), new WholeNumber(LEVEL)
 		);
 	}
 
 	private static final String ENCHANT = "enchant";
+	private static final String LEVEL = "level";
 
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
@@ -29,35 +27,16 @@ public class EnchantItem extends PlayerCommand
 		RunsafeItemStack item = executor.getItemInMainHand();
 		if (item == null)
 			return "&cNo item in your hand.";
-		StringBuilder feedback = new StringBuilder();
-		String args = parameters.getRaw(ENCHANT);
-		if (args == null)
-			return "&cNo enchants to apply";
-		String[] enchants = args.split(" ");
-		for (String param : enchants)
-		{
-			Matcher spec = enchantSpec.matcher(param);
-			IEnchant enchant;
-			if (spec.matches())
-			{
-				enchant = no.runsafe.framework.minecraft.Enchant.getByName(spec.group(1));
-				if (enchant != null)
-					enchant = enchant.power(Integer.parseInt(spec.group(2)));
-			}
-			else
-			{
-				enchant = no.runsafe.framework.minecraft.Enchant.getByName(param);
-				if (enchant != null)
-					enchant = enchant.max();
-			}
-			if (enchant != null)
-			{
-				enchant.applyTo(item);
-				feedback.append(String.format("Applied enchant %s at power %d to item.\n", enchant.getName(), enchant.power()));
-			}
-		}
-		return "&2Your item has been enchanted:\n" + feedback;
-	}
 
-	static final Pattern enchantSpec = Pattern.compile("(.*):(\\d+)");
+		IEnchant enchant = parameters.getRequired(ENCHANT);
+		Integer enchantLevel = parameters.getValue(LEVEL);
+
+		if (enchantLevel == null)
+			enchantLevel = enchant.getMaxLevel();
+		else if (enchantLevel < 1)
+			enchantLevel = 1;
+
+		enchant.power(enchantLevel).applyTo(item);
+		return String.format("&aApplied enchant %s at power %d to item in main hand.", enchant.getName(), enchant.power());
+	}
 }
